bed12b0478d8bd8778d8b1f23d08c55264734625
Timur Pavlov <pavlov.timur556@yandex.ru>
2022-10-23 20:19:49 +0300

    init: initial commit

---
+++ aiogram_pytest/handler.py
@@ -0,0 +1,99 @@
+from typing import Any
+from typing import Callable
+
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram import types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from .args_parser import ArgumentsParser
+from .types.dataset import CHAT
+from .types.dataset import USER
+from .utils import initialize_bot
+
+
+class RequestHandler:
+    def __init__(self, argument_parser: Callable):
+        self._argument_parser = argument_parser
+        self.bot = initialize_bot()
+        self.dp = Dispatcher(self.bot, storage=MemoryStorage())
+
+        Bot.set_current(self.bot)
+        Dispatcher.set_current(self.dp)
+        types.User.set_current(USER.as_object())
+        types.Chat.set_current(CHAT.as_object())
+
+    def parse_args(self, call_args: Any) -> dict:
+        if call_args is None:
+            return {}
+
+        return self._argument_parser(call_args)
+
+    async def __call__(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class MessageHandler(RequestHandler):
+    def __init__(
+        self,
+        callback,
+        *custom_filters: Any,
+        commands=None,
+        regexp=None,
+        content_types=None,
+        state=None,
+        run_rask=None,
+        state_data: dict = None,
+        **kwargs,
+    ):
+        super().__init__(ArgumentsParser.get_send_message_args)
+        self._callback = callback
+        self._custom_filters = custom_filters
+        self._commands = commands
+        self._regexp = regexp
+        self._content_types = content_types
+        self._state = state
+        self._run_task = run_rask
+        self._state_data = state_data
+
+        if self._state_data is None:
+            self._state_data = {}
+
+    async def __call__(self, message: types.Message):
+        self.dp.register_message_handler(
+            self._callback,
+            *self._custom_filters,
+            commands=self._commands,
+            regexp=self._regexp,
+            content_types=self._content_types,
+            state=self._state,
+            run_task=self._run_task,
+        )
+        if self._state:
+            await self.dp.current_state().set_state(self._state)
+            await self.dp.current_state().update_data(**self._state_data)
+
+        await self.dp.process_update(types.Update(message=message))
+
+
+class CallbackQueryHandler(RequestHandler):
+    def __init__(self, callback, *custom_filters, state=None, run_task=None, state_data: dict = None, **kwargs):
+        super().__init__(ArgumentsParser.get_answer_callback_query_args)
+        self._callback = callback
+        self._custom_filters = custom_filters
+        self._state = state
+        self._run_task = run_task
+        self._state_data = state_data
+
+        if self._state_data is None:
+            self._state_data = {}
+
+    async def __call__(self, callback_query: types.CallbackQuery):
+        self.dp.register_callback_query_handler(
+            self._callback, *self._custom_filters, state=self._state, run_task=self._run_task
+        )
+        if self._state:
+            await self.dp.current_state().set_state(self._state)
+            await self.dp.current_state().update_data(**self._state_data)
+
+        await self.dp.process_update(types.Update(callback_query=callback_query))
---
+++ tests/test_args_parser.py
@@ -0,0 +1,71 @@
+import unittest
+
+from aiogram_pytest.args_parser import ArgumentsParser
+from aiogram_pytest.utils import initialize_bot
+
+
+class TestArgsParser(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        self._bot = initialize_bot()
+
+    async def test_converting(self):
+        await self._bot.send_message(chat_id=12345678, text="Hello, Bot!", parse_mode="Markdown")
+        args = ArgumentsParser.get_send_message_args(self._bot.send_message.call_args)
+        self.assertEqual(
+            args.as_dict(),
+            {
+                "chat_id": 12345678,
+                "text": "Hello, Bot!",
+                "parse_mode": "Markdown",
+                "entities": None,
+                "disable_web_page_preview": None,
+                "disable_notification": None,
+                "protect_content": None,
+                "reply_to_message_id": None,
+                "allow_sending_without_reply": None,
+                "reply_markup": None,
+                "payload": None,
+                "result": None,
+            },
+        )
+
+    async def test_converting_with_none(self):
+        args = ArgumentsParser.get_send_message_args(None)
+        self.assertEqual(
+            args.as_dict(),
+            {
+                "chat_id": None,
+                "text": None,
+                "parse_mode": None,
+                "entities": None,
+                "disable_web_page_preview": None,
+                "disable_notification": None,
+                "protect_content": None,
+                "reply_to_message_id": None,
+                "allow_sending_without_reply": None,
+                "reply_markup": None,
+                "payload": None,
+                "result": None,
+            },
+        )
+
+    async def test_converting_with_incorrect_args(self):
+        await self._bot.send_message(arg1="arg1", arg2="arg2")
+        args = ArgumentsParser.get_send_message_args(self._bot.send_message.call_args)
+        self.assertEqual(
+            args.as_dict(),
+            {
+                "chat_id": None,
+                "text": None,
+                "parse_mode": None,
+                "entities": None,
+                "disable_web_page_preview": None,
+                "disable_notification": None,
+                "protect_content": None,
+                "reply_to_message_id": None,
+                "allow_sending_without_reply": None,
+                "reply_markup": None,
+                "payload": None,
+                "result": None,
+            },
+        )
